b6f704b1f6a1d642034e8303be428073426c0f17
Maryam Yumna <67947046+Maryam-Yumna@users.noreply.github.com>
2022-04-06 02:34:33 +0530

    notes backend

--- api/api.js
+++ api/api.js
@@ -2,6 +2,7 @@ let router = require("express").Router();
 
 //Controllers
 const alarmController = require("../controllers/alarm_controller");
+const noteController = require("../controllers/note_controller");
 
 //home route
 router.get("/", (req, res) => {
@@ -14,5 +15,13 @@ router.route("/alarms/:id").get(alarmController.getAlarmById);
 router.route("/alarms").post(alarmController.addAlarm);
 router.route("/alarms/:id").delete(alarmController.deleteAlarm);
 router.route("/alarms/:id").put(alarmController.updateAlarm);
+
+
+//Note routes
+router.route("/notes").get(noteController.getNotes);
+router.route("/notes/:id").get(noteController.getNoteById);
+router.route("/notes").post(noteController.addNote);
+router.route("/notes/:id").delete(noteController.deleteNote);
+router.route("/notes/:id").put(noteController.updateNote);
 
 module.exports = router;
